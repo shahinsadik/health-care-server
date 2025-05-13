048ac8f461b2c2d338f69f6d28effcae798a3cd2
shahinsadik <shahinsadik@gmail.com>
2025-05-13 14:28:22 +0600

    Specialties functionality added

--- src/app/modules/Specialties/specialties.routes.ts
+++ src/app/modules/Specialties/specialties.routes.ts
@@ -1,6 +1,7 @@
 import express, { NextFunction, Request, Response } from "express";
 import { SpecialtiesController } from "./specialties.controller";
 import { FileUploaders } from "../../../helpers/fileUploaders";
+import { SpecialtiesValidation } from "./specialties.validation";
 
 const router = express.Router();
 
@@ -8,7 +9,7 @@ router.post(
   "/",
   FileUploaders.upload.single("file"),
   (req: Request, res: Response, next: NextFunction) => {
-    req.body = JSON.parse(req.body.data);
+    req.body = SpecialtiesValidation.create.parse(JSON.parse(req.body.data));
     return SpecialtiesController.insertIntoDB(req, res, next);
   }
 );
---
+++ src/app/modules/Specialties/specialties.validation.ts
@@ -0,0 +1,10 @@
+import { z } from "zod";
+
+const create = z.object({
+  title: z.string({
+    required_error: "Title Is Required",
+  }),
+});
+export const SpecialtiesValidation = {
+  create,
+};
