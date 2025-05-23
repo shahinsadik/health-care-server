5d8577876f46935d7a7a87d5b2a969b4fa5969e9
shahinsadik <shahinsadik@gmail.com>
2025-05-23 07:43:13 +0600

    create appointment

--- src/app/modules/Appiontmrnt/appiontment.service.ts
+++ src/app/modules/Appiontmrnt/appiontment.service.ts
@@ -1,16 +1,42 @@
 import prisma from "../../../sheared/prisma";
 import { IAuthUser } from "../../interfaces/common";
-
-
-const createAppointment = async (user:IAuthUser, payload: any) => {
-    const patientData = await prisma.patient.findUniqueOrThrow({
+import { v4 as uuidv4 } from "uuid";
+const createAppointment = async (user: IAuthUser, payload: any) => {
+  const patientData = await prisma.patient.findUniqueOrThrow({
     where: {
       email: user?.email,
     },
-    })
-  console.log("patientData data:", patientData, payload);
-  
-  
+  });
+  const doctorData = await prisma.doctor.findUniqueOrThrow({
+    where: {
+      id: payload.doctorId,
+    },
+  });
+
+  const doctorScheduleData = await prisma.doctorSchedules.findFirstOrThrow({
+    where: {
+      doctorId: doctorData.id,
+      scheduleId: payload.scheduleId,
+      isBooked: false,
+    },
+  });
+  const videoCallingId = uuidv4();
+  const result = await prisma.appointment.create({
+    data: {
+      patientId: patientData.id,
+      doctorId: doctorData.id,
+      scheduleId: payload.scheduleId,
+      videoCallingId,
+    },
+    include:{
+        patient:true,
+        doctor:true,
+        schedule:true,
+    }
+    
+});
+
+return result;
 };
 
-export const AppointmentService = {createAppointment}
+export const AppointmentService = { createAppointment };
