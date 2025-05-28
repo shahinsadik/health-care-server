289814c6a7077743759e814ee5506a4d1bec681f
shahinsadik <shahinsadik@gmail.com>
2025-05-29 00:21:37 +0600

    Global error handler update

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import status from "http-status";
+import { Prisma } from "../../generated/prisma";
 
 const globalErrorHandeler = (
   err: any,
@@ -7,10 +8,30 @@ const globalErrorHandeler = (
   res: Response,
   next: NextFunction
 ) => {
-  res.status(status.INTERNAL_SERVER_ERROR).send({
-    success: false,
-    message: err.message || "Some thing went worng!",
-    error: err || "Internal Server Error",
+  let statusCode = status.INTERNAL_SERVER_ERROR;
+  let success = false;
+  let message = "Something went wrong!";
+  let error = err;
+
+  if (err instanceof Prisma.PrismaClientValidationError) {
+    message = "Validation Error";
+    error = err.message;
+  } else if (err instanceof Prisma.PrismaClientKnownRequestError) {
+    if (err.code === "P2002") {
+      message = "Unique constraint failed";
+      error = err.meta;
+    } else if (err.code === "P2025") {
+      message = "Record not found";
+      error = err.message;
+    } else {
+      message = "Database Error";
+      error = err.message;
+    }
+  }
+  res.status(statusCode).send({
+    success,
+    message,
+    error,
   });
 };
 export default globalErrorHandeler;
