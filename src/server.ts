8e348a2accc21c848d73f14d21fe9b631129b5cf
shahinsadik <shahinsadik@gmail.com>
2025-05-29 09:43:35 +0600

    finished

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,31 @@
-import { log } from 'console';
+import { log } from "console";
 
-
-import app from './app';
-import { Server } from 'http';
-import config from './config';
-
+import app from "./app";
+import { Server } from "http";
+import config from "./config";
 
 const port = config.port;
 
 async function main() {
-    const server: Server = app.listen(port, () => {
-        log(`Health Care Server is running on port ${port}`);
-    })
+  const server: Server = app.listen(port, () => {
+    log(`Health Care Server is running on port ${port}`);
+  });
+
+  const existHandler = () => {
+    if (server) {
+      server.close(() => {
+        log("Server closed gracefully");
+      });
+    }
+    process.exit(0);
+  };
+  process.on("uncaughtException", (err) => {
+    log("Uncaught Exception:", err);
+    existHandler();
+  });
+  process.on("unhandledRejection", (err) => {
+    log("Uncaught Exception:", err);
+    existHandler();
+  });
 }
 main();
--- src/sheared/prisma.ts
+++ src/sheared/prisma.ts
@@ -3,31 +3,31 @@ import { PrismaClient } from "../generated/prisma";
 const prisma = new PrismaClient({
   log: [
     {
-      emit: 'event',
-      level: 'query',
+      emit: "event",
+      level: "query",
     },
     {
-      emit: 'stdout',
-      level: 'error',
+      emit: "stdout",
+      level: "error",
     },
     {
-      emit: 'stdout',
-      level: 'info',
+      emit: "stdout",
+      level: "info",
     },
     {
-      emit: 'stdout',
-      level: 'warn',
+      emit: "stdout",
+      level: "warn",
     },
   ],
-})
+});
 
-prisma.$on('query', (e) => {
-    console.log('------------------------ ')
-  console.log('Query: ' + e.query)
-  console.log('------------------------ ')
-  console.log('Params: ' + e.params)
-  console.log('------------------------ ')
-  console.log('Duration: ' + e.duration + 'ms')
-  console.log('------------------------ ')
-})
+prisma.$on("query", (e) => {
+  console.log("------------------------ ");
+  console.log("Query: " + e.query);
+  console.log("------------------------ ");
+  console.log("Params: " + e.params);
+  console.log("------------------------ ");
+  console.log("Duration: " + e.duration + "ms");
+  console.log("------------------------ ");
+});
 export default prisma;
